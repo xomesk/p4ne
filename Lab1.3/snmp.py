09c63619b275cd49f332b2b79e2640c624a72c52
Vladimir shchipitcin <vladimir.shchipitcin@domru.ru>
2022-05-25 20:13:34 +0500

    add Lab1.3

---
+++ Lab1.3/snmp.py
@@ -0,0 +1,36 @@
+from pysnmp.hlapi import *
+
+
+snmp_engene = SnmpEngine()
+community_data = CommunityData("public", mpModel=0)
+transport = UdpTransportTarget(("10.31.70.107",161))
+snmp_object_version = ObjectIdentity('SNMPv2-MIB', 'sysDescr', 0)
+snmp_objetc_interface = ObjectIdentity('1.3.6.1.2.1.2.2.1.2')
+
+
+resultGet = getCmd(SnmpEngine(),
+           CommunityData('public', mpModel=0),
+           UdpTransportTarget(('10.31.70.107', 161)),
+           ContextData(),
+           ObjectType(snmp_object_version))
+
+resultNext  = nextCmd(snmp_engene,
+            community_data,
+            transport,
+            ContextData(),
+            ObjectType(snmp_objetc_interface),
+            lexicographicMode=False)
+
+
+for x in resultGet:
+    for j in x[3]:
+     print(j)
+
+for x in resultNext:
+    for j in x[3]:
+        print(j)
+
+
+
+
+
